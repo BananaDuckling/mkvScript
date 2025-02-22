faf715f9d64f2b843a7a41618f169d2fa7f4e8e9
Duck <bryantlopez2394@gmail.com>
2025-02-21 20:52:02 -0800

    Moved the Track class to a new file

--- mkvSemiAuto.py
+++ mkvSemiAuto.py
@@ -3,45 +3,13 @@ import os
 from json import loads
 import tkinter as tk
 import pandas as pd
+from _track import Track
 
 ### Creating a priority list for subtitles and audios
 videoPriority = []
 audioCodecPriority = []
 subtitleDistroPriority = ["SubStationAlpha", "SRT" , "PGS", "Vort"]
 
-
-
-###Stores the track properties as a data frame
-
-class Track:
-    def __init__(self, name:str, category:str, id:int, lang:str, priority:int, default:bool, forced:bool, enabled:bool, fileFlag:bool = False, fileName:str = ''):
-        self.fileName = fileName
-        self.fileFlag = fileFlag
-        self.name = name
-        self.id = id
-        self.category = category
-        self.priority = priority
-        self.language = lang
-        self.default = int(default)
-        self.forced = int(forced)
-        self.enabled = int(enabled)
-        
-    def cmd(self):
-        if self.fileFlag == True: #Checks if the command should include the file name
-            if os.path.isfile(self.fileName) == False:
-                raise Exception (f'ERROR. File name : "{self.fileName}" could not be found.')
-            elif not(self.fileName.endswith('.mkv'))    :
-                raise Exception (f'ERROR. File "{self.fileName}" is not a Matroska video file')
-            _cmd = f'mkvmerge "{self.fileName}" -e track=:{self.id + 1}'
-        else:
-            _cmd = f' -e track=:{self.id + 1}'
-        for i, (attr, val) in enumerate(self.__dict__.items()): #Converts properties into an executable string
-            if (i > 6):
-                _cmd += f" -s flag-{attr}={val}" 
-            elif (i > 5):
-                _cmd += f" -s {attr}={val}"          
-        return _cmd
-        
 
 def mkvCheck(file: str): #will be used for checking which case of the mkv file is used
     output = run(args = ['mkvmerge','-J', file], capture_output = True, check = True)
